d447a0554dc7d62785aa5b884ee9fca4a7518c5f
Jeremy Lacomis <j.lacomis@gmail.com>
2024-11-19 15:49:20 -0500

    Update Manager.java

--- src/main/java/edu/cmu/DirManager/Manager.java
+++ src/main/java/edu/cmu/DirManager/Manager.java
@@ -9,8 +9,7 @@ public class Manager {
      * @param path the path where the new directory should be created
      * @return 0 if the directory creation was successful
      *        -1 if the directory already exists,
-     *        -2 if the path is invalid,
-     *        -3 if the directory could not be created for some other reason
+     *        -2 if the path is invalid
      */
     public int newDirectory(String path) {
         if (dirOps.checkDirectoryExists(path)) {
